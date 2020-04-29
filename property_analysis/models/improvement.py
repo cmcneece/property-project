6620675a8676d50e4502f9195017bcc2cd06080e
cmcneece <13018191+cmcneece@users.noreply.github.com>
2020-04-28 19:31:50 -0500

    Add setup file

--- property_analysis/models/improvement.py
+++ property_analysis/models/improvement.py
@@ -1,8 +1,13 @@
 from traits.api import (
-    HasStrictTraits, Str, Float,
+    HasStrictTraits, Str, Float, Int,
 )
 
 
 class Improvement(HasStrictTraits):
 
-    
+    prop_id = Int()
+
+
+if __name__ == "__main__":
+    imp = Improvement(prop_id=5)
+    imp.print_traits()
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='property_analysis',
+      version='0.1',
+      description='Tools to import and analyze property data.',
+      url='https://github.com/cmcneece/property_analysis',
+      author='Kindra Nicholaides',
+      author_email='ardnik@gmail.com',
+      license='BSD2',
+      packages=['property_analysis'],
+      zip_safe=False)
